bb9d4fcae767d7223f948ceed15b24b019120d43
jpcastro14 <jpcastro14@hotmail.com>
2021-12-06 14:28:23 -0300

    Alterando quantidade de posts para 5

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -20,7 +20,7 @@ export class Home extends Component {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 3,
+    postsPerPage: 5,
     searchValue: "",
   };
   componentDidMount() {
